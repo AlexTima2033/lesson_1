8ab4a1307fa1fa7ac7126be8e561fe25e01901d1
Alex Gub <sashatimofeeva2003@gmail.com>
2026-01-15 12:56:05 +0300

    Дз 4

---
+++ homework_3.py
@@ -0,0 +1,14 @@
+"""fruits = ["apple", "banana", "cherry"]
+del fruits [1]
+print(fruits)"""
+
+"""fruits = ["apple", "banana", "cherry"]
+del_element = fruits.pop(0)
+print (fruits)
+print (del_element)"""
+
+fruits = ["apple", "banana", "cherry"]
+nefruits = "cherry"
+fruits.remove(nefruits)
+print(fruits)
+
