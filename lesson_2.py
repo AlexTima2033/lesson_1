45a0f919fecfa13b34c8d77cd763716f279c668a
Alex Gub <sashatimofeeva2003@gmail.com>
2026-01-16 15:41:37 +0300

    Урок 3

---
+++ lesson_2.py
@@ -0,0 +1,30 @@
+
+"""fruits.sort(reverse=True)"""
+"""print(fruits)"""
+"""fruits.reverse()"""
+
+"""for fruit in fruits:
+    print(f"{fruit.title()}")
+print("Это все фрукты и ягоды")"""
+
+"""for value in range(1, 10):
+    print(value)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+
+squares = []
+for value in range(1, 11):
+
+    squares.append(value**2)
+print(squares)"""
+
+"""fruits = ["cherry", "banana", "apple", "peach", "grape", "winegrape", "mango"]
+print(fruits[-1:])"""
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+print(numbers)
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+number[0] = 22
+print(number)
