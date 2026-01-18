e7c115a968d8d770ad072790c5f69a5c92c6963a
Alex Gub <sashatimofeeva2003@gmail.com>
2026-01-18 17:10:43 +0300

    Добавила ещё способ

--- homework_4.py
+++ homework_4.py
@@ -1,5 +1,10 @@
-cubes = []
+"""cubes = []
 for value in range(1, 12):
     cube = value**3
     cubes.append(cube)
+print(cubes) """
+
+cubes = []
+for value in range(1, 12):
+    cubes.append(value**3)
 print(cubes) 
