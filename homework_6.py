17910bd25319a67a67aecb5a0616629e2047963f
Alex Gub <sashatimofeeva2003@gmail.com>
2026-01-18 17:45:33 +0300

    Фруктовая лавка

---
+++ homework_6.py
@@ -0,0 +1,11 @@
+fruits = ["grape", "apple", "banana", "orange","kiwi"]
+fruits.insert(0,"mango")
+print(fruits)
+fruits.append("peach")
+print(fruits)
+fruits[4]= "mandarin"
+print(fruits)
+for fruit in fruits:
+    print(fruit)
+print("Содержимое фруктовой лавки")
+
