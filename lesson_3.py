55ad95f68ae5617b17ae490475650176f0ea1fea
Alex Gub <sashatimofeeva2003@gmail.com>
2026-01-28 14:32:05 +0300

    Урок 4

---
+++ lesson_3.py
@@ -0,0 +1,32 @@
+"""name = str(input("Введите ваше имя: "))
+age = int(input("Введите ваш возраст: "))
+message = f"Привет, {name.title()}!\nТвой возраст - {age}."
+print(message)
+print(type(name))
+print(type(age))
+
+
+"""
+"""number = float(input("Введите число: "))
+number_1 = float(input("Введите второе число: "))
+result = number + number_1
+print(result)"""
+
+"""numbers = (1, 3, 8, 4, 15, 54, 7, 2, 9, 10)
+print ("Это числа из кортежа после цикла for:\n")
+for i in numbers:
+    print(i)"""
+
+"""fruits = ["Apple", "Banana", "Cherry"]
+for i in fruits:
+    if i == "apple":
+        print(i.upper())
+    else:
+        print(i.title())"""
+
+age = 19
+if age != 18:
+    print("Тебе не можно пиво")
+else:
+    print("Тебе можно пиво")
+
