72ce146c719fe86ded047c1753c3d5bd2c212d14
Alex Gub <sashatimofeeva2003@gmail.com>
2026-01-29 16:38:35 +0300

    28.01

--- homework_8.py
+++ homework_8.py
@@ -12,3 +12,8 @@ print(result) """
 number_2 = int(input("Введите второе число: "))
 result = number_1 // number_2
 print(result)"""
+
+number_1 = float(input("Введите первое число: "))
+number_2 = float(input("Введите второе число: "))
+result = number_1 + number_2
+print(result)
