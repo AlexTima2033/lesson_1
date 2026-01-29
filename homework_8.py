0fd838b1c3bebb6f5cea7d0f3d6ba6407729f90a
Alex Gub <sashatimofeeva2003@gmail.com>
2026-01-29 16:33:05 +0300

    28.01

---
+++ homework_8.py
@@ -0,0 +1,14 @@
+"""number_1 = int(input("Введите первое число: "))
+number_2 = int(input("Введите второе число: "))
+result = number_1 - number_2
+print(result) """
+
+"""number_1 = int(input("Введите первое число: "))
+number_2 = int(input("Введите второе число: "))
+result = number_1 * number_2
+print(result) """
+
+"""number_1 = int(input("Введите первое число: "))
+number_2 = int(input("Введите второе число: "))
+result = number_1 // number_2
+print(result)"""
