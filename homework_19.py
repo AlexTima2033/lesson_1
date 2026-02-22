2f79922fbc70a6ab41291ef27779db35e801961c
Alex Gub <sashatimofeeva2003@gmail.com>
2026-02-22 22:12:47 +0300

    Дз от урока 18.02 к уроку 23.02

---
+++ homework_19.py
@@ -0,0 +1,16 @@
+"""def math(a,b,c):
+   return a + b - c
+result = math(1,2,3)
+print(result)"""
+
+"""def math(a,b):
+   return a // b
+result = math(25,5)
+print(result)"""
+
+def math(a,b,c):
+   return a // b - 10
+result = math(25,5,10)
+print(result)
+
+
